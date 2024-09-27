3299dd4477e8401913ac59b0abfef861715a339c
Ivan Silvosa Rodriguez <ivansrlsy@gmail.com>
2024-09-27 14:26:34 +0200

    Ejercicio Resuelto

--- src/main/java/com/campusdual/classroom/Exercise29.java
+++ src/main/java/com/campusdual/classroom/Exercise29.java
@@ -1,7 +1,16 @@
 package com.campusdual.classroom;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class Exercise29 {
     public static void main(String[] args) {
-
+        try {
+            File file = new File("archivo.txt");
+            FileReader fileReader = new FileReader(file);
+        } catch (FileNotFoundException e) {
+            System.out.println("El archivo no se encontró.");
+        }
     }
 }
